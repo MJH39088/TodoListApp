3f6f7d14a5ba06c2ff0476d051c01e1cf1ddb551
mjh39088 <gjalswo3908@gmail.com>
2022-11-16 23:04:12 +0900

    View binding 방법, lateinit

--- app/src/main/java/com/minjaeheo/todolistapp/MainActivity.kt
+++ app/src/main/java/com/minjaeheo/todolistapp/MainActivity.kt
@@ -2,12 +2,25 @@ package com.minjaeheo.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.minjaeheo.todolistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // layout 파일과 상호작용 할 수 있게 바인딩함 gradle에서 먼저 키고 써야됨. lateinit 늦은 초기화, 코틀린에선 최초로 값을 할당을 초기화라고 함
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // 초기화
+        setContentView(binding.root) // 뷰 바인딩 결합 xml과 합체 fb 구문 안 써도 됨 (보일러 플레이트)
+
+        // 카멜케이스 fb처럼 앞에 binding만 쓰면 된다.
+        binding.tvName.setText("안녕하세요 연습중")
+        binding.tvTest.setText("연습중2")
+
+        // log 찍는 방법
+        // Log.d("!@", "처음 호출될 때  ${setContentView(R.layout.activity_main)}")
 
         // 액티비티가 생성될 때 호출되며 사용자 인터페이스 초기화 할 때 이 곳에 구현
         println("minjaeheo onCreate")
